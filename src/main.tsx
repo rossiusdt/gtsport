62cd71c59e4318f732ac58b8acfbd7de01fbf494
rossiusdt <vitorfonsecaprofissional@gmail.com>
2026-05-12 17:18:31 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import Admin from './pages/Admin.tsx';
 import './index.css';
-
-const isAdmin = window.location.pathname === '/admin';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isAdmin ? <Admin /> : <App />}
+    <App />
   </StrictMode>
 );
